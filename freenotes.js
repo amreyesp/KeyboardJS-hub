299e43335816f93a6de48331b2b498e756b258b8
amreyesp <amreyesp@unal.edu.co>
2019-04-09 11:24:25 -0500

    create a dependency to free mode play and implement event listener to identify the key pressed by user

---
+++ freenotes.js
@@ -0,0 +1,39 @@
+
+function PlayKeyboard() {
+  C.addEventListener('click',function() {
+    console.log(`This is ${C.id} note`)
+  })
+  Csharp.addEventListener('click',function() {
+    console.log(`This is ${Csharp.id} note`)
+  })
+  D.addEventListener('click',function() {
+    console.log(`This is ${D.id} note`)
+  })
+  Dsharp.addEventListener('click',function() {
+    console.log(`This is ${Dsharp.id} note`)
+  })
+  E.addEventListener('click',function() {
+    console.log(`This is ${E.id} note`)
+  })
+  F.addEventListener('click',function() {
+    console.log(`This is ${F.id} note`)
+  })
+  Fsharp.addEventListener('click',function() {
+    console.log(`This is ${Fsharp.id} note`)
+  })
+  G.addEventListener('click',function() {
+    console.log(`This is ${G.id} note`)
+  })
+  Gsharp.addEventListener('click',function() {
+    console.log(`This is ${Gsharp.id} note`)
+  })
+  A.addEventListener('click',function() {
+    console.log(`This is ${A.id} note`)
+  })
+  Asharp.addEventListener('click',function() {
+    console.log(`This is ${Asharp.id} note`)
+  })
+  B.addEventListener('click',function() {
+    console.log(`This is ${B.id} note`)
+  })
+}
