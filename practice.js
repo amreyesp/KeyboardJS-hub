4fa98f6d15d1b51f3b274cfcdda76c35a80e5f9c
amreyesp <amreyesp@unal.edu.co>
2019-04-19 13:54:21 -0500

    include tapped key revision (checkTabMatch) once the user tapped each key on practice mode

--- practice.js
+++ practice.js
@@ -19,18 +19,10 @@ class Practice {
       closeOnClickOutside: false,
       closeOnEsc: false,
       buttons: {
-        song1: {
-          text: "Song 1", value: "song1",
-        },
-        song2: {
-          text: "Song 2", value: "song2",
-        },
-        song3: {
-          text: "Song 3", value: "song3",
-        },
-        other: {
-          text: "No, I want a random song", value: "random",
-        }
+        song1: { text: "Song 1", value: "song1" },
+        song2: { text: "Song 2", value: "song2" },
+        song3: { text: "Song 3", value: "song3" },
+        other: { text: "No, I want a random song", value: "random" }
       },
     })
     .then(value => {
@@ -56,7 +48,6 @@ class Practice {
           break;
         case 2:
           song = 'song3';
-          break;
       }
     }
     switch (song) {
@@ -68,7 +59,6 @@ class Practice {
         break;
       case 'song3':
         tabs = [C, C, F, F, G, G, F, F, F, E, E, D, D, C];
-        break;
     }
     console.log(tabs);
   }
@@ -82,38 +72,29 @@ function PracticeSession() {
 function StartPractice() {
   //Define number of levels for each song: number of key tabs
   const maxLevel = tabs.length;
-  var level = 4;
+  var level = 3;
   var practiceStatus = 'on practice';
   //Practice on each level of the song (e.g. level 0 = key0; level 1 = key0 + key1 ...)
-  while (practiceStatus === 'on practice') {
-    practiceStatus = PracticeOnLevel(level, maxLevel);
-    EvalPractice(practiceStatus);
-    level++;
-  }
+  practiceStatus = PracticeOnLevel(level, maxLevel);
+  //EvalPractice(practiceStatus);
+  // while (practiceStatus === 'on practice') {
+  //   practiceStatus = PracticeOnLevel(level, maxLevel);
+  //   EvalPractice(practiceStatus);
+  //   level++;
+  // }
 }
-
-// function LightOnKey(keyToLight) {
-//   keyToLight.classList.add('light');
-//   }
-//
-// function LightOutKey(keyToLightOut) {
-//   keyToLightOut.classList.remove('light');
-// }
 
 function toggleLightKey(key) {
   key.classList.add('light');
-  setTimeout(() => {key.classList.add('light'); key.classList.remove('light')}, 300);
+  setTimeout(() => {key.classList.remove('light')}, 300);
 }
 
 //Implement practice on specific level
 function PracticeOnLevel(level, maxLevel) {
-  var tabsTapped;
   if (level < maxLevel) {
-      PlayArray(level);
-      //Compare keys tapped vs elements of array
-      //Update practiceStatus variable
-      //Increment practice level
-      practiceStatus = 'on practice';
+    practiceStatus = 'on practice';
+    PlayArray(level)
+    CaptureKeyEvent(level);
   } else {
     practiceStatus = 'succeded';
   }
@@ -124,10 +105,38 @@ function PlayArray(level) {
   for (let j = 0; j <= level; j++) {
     setTimeout(() => { GetSoundKey(tabs[j]).play(); toggleLightKey(tabs[j]) }, 1000 * j);
   }
+  setTimeout(() => {console.log('Song ended')} , 1000 * level)
+}
+
+function onError() {
+  console.log(`An error ocurred`);
+}
+
+function CaptureKeyEvent(level) {
+  C.addEventListener('click', CheckTabMatch);
+  D.addEventListener('click', CheckTabMatch);
+  E.addEventListener('click', CheckTabMatch);
+  F.addEventListener('click', CheckTabMatch);
+  G.addEventListener('click', CheckTabMatch);
+  A.addEventListener('click', CheckTabMatch);
+
+  function CheckTabMatch() {
+    tappedKey = event.target;
+    GetSoundKey(tappedKey).play();
+    if (tapCounter <= level) {
+      if (tappedKey === tabs[tapCounter]) {
+        console.log(`${tappedKey.id} event captured`)
+        tapCounter++;
+      } else {
+        onError();
+        EvalPractice('Practice over');
+      }
+    }
+  }
 }
 
 function EvalPractice(practiceStatus) {
-  console.log(practiceStatus);
+  console.log(`Eval practice ${practiceStatus}`);
   if (practiceStatus === 'succeded'){
     //swal("Congratulations!",`You learn the ${practice.selectedSong}`, "success");
   }
